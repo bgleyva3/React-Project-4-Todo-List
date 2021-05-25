a5f8abc54b59958a6e1c094e3005617c495c21c5
Brandon Leyva <bgleyva3@gmail.com>
2021-05-24 19:45:23 -0500

    some fixes in style

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -5,13 +5,17 @@ const CreateTodo = ({handleCreate}) => {
     const {handleSubmit, register} = useForm()
 
     return (
-        <form className="row form-margins" onSubmit={handleSubmit(handleCreate)} >
-            <div className="col-sm-1"></div>
-            <input className="col col-sm no-border" placeholder="Task" {...register("task", { required: true })} ></input>
-            <input className="col col-sm-2 no-border separate" placeholder="Student" {...register("student", { required: true })} ></input>
-            <button className="col-sm-auto btn btn-success more-size" type="submit" ><i class="fas fa-plus"></i></button>
-            <div className="col-sm-1"></div>
-        </form>
+        <div className="row">
+            <div className="col-1 col-sm-1"></div>
+            <div className="col-10 col-sm-10 no-padding">
+                <form className="row form-margins" onSubmit={handleSubmit(handleCreate)} >
+                    <input className="col col-sm no-border" placeholder="Task" {...register("task", { required: true })} ></input>
+                    <input className="col col-sm-2 no-border margin-left" placeholder="Student" {...register("student", { required: true })} ></input>
+                    <button className="col-12 col-sm-auto btn btn-success more-size separate" type="submit" ><i className="fas fa-plus"></i></button>
+                </form>
+            </div>
+            <div className="col-1 col-sm-1"></div>
+        </div>
     )
 }
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,25 +4,25 @@ const TodoItem = ({task, student, handleDelete, isCompleted, handleUpdate}) => {
     
     return(
         <div className="row margins ">
-            <div className="col-sm"></div>
-            <div className="col col-sm-10">
+            <div className="col-1 col-sm-1"></div>
+            <div className="col-10 col-sm-10">
                 <div className="row">
-                    <div className="col col-sm task-box no-overflow">
-                        <p><span className="task-text no-overflow">"{task}"</span> <span className="student-text">by {student}</span></p>
+                    <div className="col-12 col-sm task-box no-overflow">
+                        <p><span className="task-text">"{task}"</span> <span className="student-text">by {student}</span></p>
                     </div>
-                    <div className="col col-sm-auto align-self-center no-padding">
+                    <div className="col-12 col-sm-auto separate align-self-center no-padding margin-buttons">
                         {
                             isCompleted ? 
                                 <button className="btn btn-success" onClick={handleUpdate} >Completed</button>
                                 :
                                 <button className="btn btn-warning" onClick={handleUpdate} >Pending</button>
                         }
-                        <button className="btn btn-danger" onClick={handleDelete} ><i class="fas fa-trash"></i></button>
+                        <button className="btn btn-danger" onClick={handleDelete} ><i className="fas fa-trash"></i></button>
                     </div>
                 </div>
                 
             </div>
-            <div className="col-sm"></div>
+            <div className="col-1 col-sm-1"></div>
         </div>
         
     )
