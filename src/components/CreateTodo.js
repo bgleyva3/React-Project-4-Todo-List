fc35d6db30cfe3ddbf3b2277c0dd7c2e25c4c600
Brandon Leyva <bgleyva3@gmail.com>
2021-05-24 20:14:02 -0500

    fixed some things

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -6,15 +6,15 @@ const CreateTodo = ({handleCreate}) => {
 
     return (
         <div className="row">
-            <div className="col-1 col-sm-1"></div>
-            <div className="col-10 col-sm-10 no-padding">
+            <div className="col-1 col-sm-1 col-md-2 col-lg-3"></div>
+            <div className="col-10 col-sm-10 col-md-8 col-lg-6 no-padding">
                 <form className="row form-margins" onSubmit={handleSubmit(handleCreate)} >
                     <input className="col col-sm no-border" placeholder="Task" {...register("task", { required: true })} ></input>
                     <input className="col col-sm-2 no-border margin-left" placeholder="Student" {...register("student", { required: true })} ></input>
                     <button className="col-12 col-sm-auto btn btn-success more-size separate" type="submit" ><i className="fas fa-plus"></i></button>
                 </form>
             </div>
-            <div className="col-1 col-sm-1"></div>
+            <div className="col-1 col-sm-1 col-md-2 col-lg-3"></div>
         </div>
     )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,11 @@ const TodoItem = ({task, student, handleDelete, isCompleted, handleUpdate}) => {
     
     return(
         <div className="row margins ">
-            <div className="col-1 col-sm-1"></div>
-            <div className="col-10 col-sm-10">
+            <div className="col-1 col-sm-1 col-md-2 col-lg-3"></div>
+            <div className="col-10 col-sm-10 col-md-8 col-lg-6">
                 <div className="row">
-                    <div className="col-12 col-sm task-box no-overflow">
-                        <p><span className="task-text">"{task}"</span> <span className="student-text">by {student}</span></p>
+                    <div className="col-12 col-sm d-flex align-items-center justify-content-center task-box no-overflow fix-center">
+                        <p className="mb-0"><span className="task-text">"{task}"</span> <span className="student-text">by {student}</span></p>
                     </div>
                     <div className="col-12 col-sm-auto separate align-self-center no-padding margin-buttons">
                         {
@@ -22,7 +22,7 @@ const TodoItem = ({task, student, handleDelete, isCompleted, handleUpdate}) => {
                 </div>
                 
             </div>
-            <div className="col-1 col-sm-1"></div>
+            <div className="col-1 col-sm-1 col-md-2 col-lg-3"></div>
         </div>
         
     )
