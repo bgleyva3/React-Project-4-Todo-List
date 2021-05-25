03075f29323b7c8d10ecfb553a7f226f18f32635
Brandon Leyva <bgleyva3@gmail.com>
2021-05-24 22:29:42 -0500

    ye

--- src/components/CreateTodo.js
+++ src/components/CreateTodo.js
@@ -9,8 +9,8 @@ const CreateTodo = ({handleCreate}) => {
             <div className="col-1 col-sm-1 col-md-2 col-lg-3"></div>
             <div className="col-10 col-sm-10 col-md-8 col-lg-6 no-padding">
                 <form className="row form-margins" onSubmit={handleSubmit((e) => handleCreate(e,reset))} >
-                    <input className="col col-sm no-border" placeholder="Task" {...register("task", { required: true })} ></input>
-                    <input className="col col-sm-2 no-border margin-left" placeholder="Student" {...register("student", { required: true })} ></input>
+                    <input className="col col-sm no-border border-radius" placeholder="Task" {...register("task", { required: true })} ></input>
+                    <input className="col col-sm-2 no-border margin-left border-radius" placeholder="Student" {...register("student", { required: true })} ></input>
                     <button className="col-12 col-sm-auto btn btn-success more-size separate" type="submit" ><i className="fas fa-plus"></i></button>
                 </form>
             </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,7 @@ const TodoItem = ({task, student, handleDelete, isCompleted, handleUpdate}) => {
             <div className="col-1 col-sm-1 col-md-2 col-lg-3"></div>
             <div className="col-10 col-sm-10 col-md-8 col-lg-6">
                 <div className="row">
-                    <div className={`${taskDone} col-12 col-sm d-flex align-items-center justify-content-center task-box no-overflow fix-center`}>
+                    <div className={`${taskDone} border-radius col-12 col-sm d-flex align-items-center justify-content-center task-box no-overflow fix-center`}>
                         <p className="mb-0"><span className="task-text">"{task}"</span> <span className="student-text">by {student}</span></p>
                     </div>
                     <div className="col-12 col-sm-auto separate align-self-center no-padding margin-buttons">
